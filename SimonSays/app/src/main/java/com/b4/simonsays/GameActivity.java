ae5bf57b10e1f71acfc8c7a86ff9a5a934ca0710
Arne de Beer <arnedebeer2001@gmail.com>
2020-05-26 09:25:38 +0200

    [Update] GitIgnore updated

--- SimonSays/app/src/main/java/com/b4/simonsays/GameActivity.java
+++ SimonSays/app/src/main/java/com/b4/simonsays/GameActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -89,7 +89,7 @@ public class FullscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fullscreen);
+        setContentView(R.layout.activity_game);
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
