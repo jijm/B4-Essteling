97a5a1401dd777d6346fa373a7fa789d7a303211
Arne de Beer <arnedebeer2001@gmail.com>
2020-05-28 12:55:36 +0200

    [Update] New message keys added to MQTT settings

--- SimonSays/app/src/main/java/com/b4/simonsays/mqtt/MessageListener.java
+++ SimonSays/app/src/main/java/com/b4/simonsays/mqtt/MessageListener.java
@@ -1,4 +1,7 @@
 package com.b4.simonsays.mqtt;
 
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public interface MessageListener {
+    void onMessageArrived(String topic, MqttMessage message);
 }
--- SimonSays/app/src/main/java/com/b4/simonsays/mqtt/MqttSettings.java
+++ SimonSays/app/src/main/java/com/b4/simonsays/mqtt/MqttSettings.java
@@ -5,15 +5,21 @@ import android.util.Log;
 public class MqttSettings {
     private final String LOG_TAG = this.getClass().getName();
 
-    public static final String SERVER_ADDRESS = "maxwell.bps-software.nl";
-    public static final String PORT = "1883";
-    public static final String USERNAME = "androidTI";
+    // General MQTT settings
+    private static final String SERVER_ADDRESS = "maxwell.bps-software.nl";
+    private static final String PORT = "1883";
+
+    static final String USERNAME = "androidTI";
     static final String PASSWORD = "&FN+g$$Qhm7j";
-    static final String BASE_TOPIC = "groupb4/simonsays";
-    private static final String APP_TOPIC = "app_update";
-    private static final String ESP_TOPIC = "esp_update";
+
     static final int QOS = 2;
 
+    // MQTT topic settings
+    private static final String BASE_TOPIC = "groupb4/simonsays";
+    private static final String APP_TOPIC = "app_update";
+    private static final String ESP_TOPIC = "esp_update";
+
+    // Possible messages that are expected to be sent
     public static final String RED_BUTTON_PRESSED_MESSAGE = "RED_BUTTON_PRESSED";
     public static final String YELLOW_BUTTON_PRESSED_MESSAGE = "YELLOW_BUTTON_PRESSED";
     public static final String GREEN_BUTTON_PRESSED_MESSAGE = "GREEN_BUTTON_PRESSED";
@@ -21,6 +27,14 @@ public class MqttSettings {
 
     public static final String READY_MESSAGE = "READY";
     public static final String LEAVE_MESSAGE = "LEAVE";
+
+    // Possible messages that are expected to be received
+    public static final String SHOWING_SEQUENCE_MESSAGE = "SHOWING_SEQUENCE";
+    public static final String CORRECT_MESSAGE = "CORRECT";
+    public static final String WRONG_MESSAGE = "WRONG";
+    public static final String WON_MESSAGE = "WON";
+    public static final String WAITING_FOR_INPUT_MESSAGE = "WAITING_FOR_INPUT";
+    public static final String WAITING_FOR_SEQUENCE_MESSAGE = "WAITING_FOR_SEQUENCE";
 
     MqttSettings() {
         Log.d(LOG_TAG, String.format("Initialized MqttSettings: %s, %s, %s, %s, %s, %s", SERVER_ADDRESS, PORT, USERNAME, PASSWORD, BASE_TOPIC, QOS));
@@ -34,7 +48,7 @@ public class MqttSettings {
         return String.format("%s/%s", BASE_TOPIC, APP_TOPIC);
     }
 
-    public static String getFullEspTopic() {
+    static String getFullEspTopic() {
         return String.format("%s/%s", BASE_TOPIC, ESP_TOPIC);
     }
 }
