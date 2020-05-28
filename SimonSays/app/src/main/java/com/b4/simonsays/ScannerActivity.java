1b9d3a130a30756312e989e62cfcb9d4484a4f0c
Nathalie Seen <n.seen@student.avans.nl>
2020-05-28 15:37:31 +0200

    [feature] added qr-scanner

---
+++ SimonSays/app/src/main/java/com/b4/simonsays/ScannerActivity.java
@@ -0,0 +1,112 @@
+package com.b4.simonsays;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission.CAMERA;
+
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private int requestCamera;
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+
+        if(checkPermission()){
+            //Toast.makeText(ScannerActivity.this, "ok",Toast.LENGTH_LONG).show();
+
+        }else{
+            requestPermission();
+        }
+
+
+    }
+    //checks if ScannerAcivity has permission to use camera
+    public boolean checkPermission(){
+        return (ContextCompat.checkSelfPermission(ScannerActivity.this, CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{CAMERA}, requestCamera);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String permission[], int grantResults[]) {
+        if (requestCode == requestCamera) {
+            if (grantResults.length > 0) {
+                boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                if (cameraAccepted) {
+                }else{
+                    if(shouldShowRequestPermissionRationale(CAMERA)){
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestPermissions(new String[]{CAMERA}, requestCamera);
+                                    }
+                                };
+
+                    }
+                }
+            }
+        }
+    }
+
+    //checks if there is a cscannerview, if not it creates one
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(checkPermission())
+        {
+            if(scannerView == null){
+                scannerView = new ZXingScannerView(this);
+                setContentView(scannerView);
+            }
+            scannerView.setResultHandler(this);
+            scannerView.startCamera();
+        }
+    }
+
+    //stops the camera
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        scannerView.stopCamera();
+    }
+
+    /*public void displayAlertMessage(String message, DialogInterface.OnClickListener listener){
+        AlertDialog.Builder acceptCamera = new AlertDialog.Builder(ScannerActivity.this);
+        acceptCamera.setMessage(message);
+        acceptCamera.setPositiveButton("OK", listener);
+        acceptCamera.setNegativeButton("No", null);
+        acceptCamera.create();
+        acceptCamera.show();
+    }*/
+
+
+    @Override
+    public void handleResult(Result result) {
+        if(result.getText().equals("Shining Saphires")) {
+            Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+}
--- SimonSays/app/src/main/java/com/b4/simonsays/StartFragment.java
+++ SimonSays/app/src/main/java/com/b4/simonsays/StartFragment.java
@@ -1,5 +1,6 @@
 package com.b4.simonsays;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,22 +10,24 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 public class StartFragment extends Fragment {
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
 
         view.findViewById(R.id.scan_button).setOnClickListener(view1 ->
-                NavHostFragment.findNavController(StartFragment.this)
-                .navigate(R.id.action_StartFragment_to_WaitingFragment));
+                Intent intent = new Intent(getActivity().getApplication(), ScannerActivity.class);
+                startActivity(intent);
     }
 }
