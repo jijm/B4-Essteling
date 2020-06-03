56427905ceb78f440b4affd0a2db5928c253a6b8
Arne de Beer <arnedebeer2001@gmail.com>
2020-06-03 10:04:54 +0200

    [Update] Help dialog implemented in MainActivity

--- SimonSays/app/src/main/java/com/b4/simonsays/MainActivity.java
+++ SimonSays/app/src/main/java/com/b4/simonsays/MainActivity.java
@@ -1,8 +1,11 @@
 package com.b4.simonsays;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -24,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
         /*Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_help){
+            showHelpDialog();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showHelpDialog() {
+        AlertDialog.Builder helpDialog = new AlertDialog.Builder(this);
+        helpDialog.setTitle(R.string.dialog_help_title);
+        helpDialog.setMessage(R.string.dialog_help_message);
+        helpDialog.setPositiveButton(R.string.dialog_help_confirm, (dialog, which) -> dialog.dismiss());
+        helpDialog.create().show();
     }
 }
